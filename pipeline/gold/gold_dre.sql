6c87a92bd22ac3c8574f721f136667dd90d2f280
leonardocs1 <leonardo.costa.santos.si@gmail.com>
2026-01-04 20:42:04 +0000

    gold dre

---
+++ pipeline/gold/gold_dre.sql
@@ -0,0 +1,18 @@
+CREATE OR REFRESH MATERIALIZED VIEW bitz.gold.gold_dre AS
+
+SELECT
+    t1.cod_filial,
+    t1.centro_custo,
+    t1.descricao AS nome_plano,
+    t1.dtaconta,
+    t1.vlr_total,
+    t1.tipo,
+    t2.cod_plano,
+    t2.cod_raiz,
+    t2.cod_grupo,
+    t2.nivel_1,
+    t2.nivel_2,
+    t2.nivel_3
+FROM bitz.silver.silver_dre AS t1
+LEFT JOIN bitz.silver.dim_planos_dre AS t2
+       ON t1.descricao = t2.nivel_3;
