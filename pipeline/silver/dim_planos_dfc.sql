c523097f5788dfb44511cddb296e1ed2f343e5b5
leonardocs1 <leonardo.costa.santos.si@gmail.com>
2026-01-05 18:40:36 +0000

    tabela silver dim_planos_dfc

---
+++ pipeline/silver/dim_planos_dfc.sql
@@ -0,0 +1,34 @@
+CREATE OR REFRESH MATERIALIZED VIEW bitz.silver.dim_planos_dfc AS
+
+WITH tab_nivel_2 AS (
+    SELECT
+        codigo,
+        grupo,
+        nsubgrupo AS nivel_2
+    FROM bitz.raw_gin_premier.mov_vinc_dfc
+    WHERE ngrupo <> 'SubGrupo'
+),
+
+tab_nivel_3 AS (
+    SELECT
+        id,
+        codigo,
+        grupo,
+        nsubgrupo AS nivel_3
+    FROM bitz.raw_gin_premier.mov_vinc_dfc
+    WHERE ngrupo = 'SubGrupo'
+)
+
+SELECT
+    CAST(t1.id     AS INT) AS cod_plano,
+    CAST(t1.codigo AS INT) AS cod_raiz,
+    CAST(t1.grupo  AS INT) AS cod_grupo,
+    t3.nome                 AS nivel_1,
+    t2.nivel_2,
+    t1.nivel_3
+FROM tab_nivel_3 AS t1
+LEFT JOIN tab_nivel_2 AS t2
+       ON t1.codigo = t2.codigo
+      AND t1.grupo  = t2.grupo
+LEFT JOIN bitz.silver.dim_dfc_nivel_0 AS t3
+       ON t1.codigo = t3.codigo;
